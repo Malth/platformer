89537d365d95f8dc65db244577ada680003d3c44
Skitak <bas2205@live.fr>
2016-06-21 17:28:02 +0200

    Controller script

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InputController : MonoBehaviour	{
+
+	private PlayerController m_Character;
+	private bool m_Jump;
+
+	private void Awake(){
+		m_Character = GetComponent<PlayerController>();
+	}
+
+
+	private void Update()
+	{
+		if (!m_Jump)
+		{
+			m_Jump = Input.GetButtonDown("Jump");
+		}
+	}
+
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		bool crouch = Input.GetKey(KeyCode.LeftControl);
+		float h = Input.GetAxis("Horizontal");
+		// Pass all parameters to the character control script.
+		m_Character.Move(h, crouch, m_Jump);
+		m_Jump = false;
+	}
+}
