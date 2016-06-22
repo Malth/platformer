6d80126cbb0cccc446955c02c394fd84d9a5a786
Skitak <bas2205@live.fr>
2016-06-23 00:03:35 +0200

    Prefab generators

--- Assets/Scripts/Champignon.cs
+++ Assets/Scripts/Champignon.cs
@@ -5,12 +5,19 @@ public class Champignon : MonoBehaviour {
 
 	public float m_puissance; 
 
+	private Animator m_animator;
+
+	void Start(){
+		m_animator = this.GetComponent<Animator> ();
+	}
+
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Player" && other.GetComponent<Rigidbody2D>().velocity.y != 0)
 			FaireDesTrucs (other.gameObject);
 	}
 
 	void FaireDesTrucs(GameObject other){
-		other.GetComponent<Rigidbody2D> ().velocity = Vector2.up * m_puissance;	
+		other.GetComponent<Rigidbody2D> ().velocity = Vector2.up * m_puissance;
+		m_animator.SetTrigger ("Bounce");
 	}
 }
