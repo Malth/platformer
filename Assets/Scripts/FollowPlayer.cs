c57c51c5889df2368a65cddd3ab9fbe57fd0da69
Skitak <bas2205@live.fr>
2016-06-22 21:35:23 +0200

    Script follow

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	private GameObject m_player;
+
+	void Start(){
+		m_player = InputController.controller.gameObject;
+	}
+
+	void Update(){
+		Camera.main.transform.position = new Vector3 (m_player.transform.position.x, m_player.transform.position.y, Camera.main.transform.position.z);
+	}
+
+}
